0e613db1627159857975fc1cd29bfc764c428966
nbmaverick <nbdevops3036@gmail.com>
2023-10-02 20:15:23 +0000

    fmt

--- Team2/data.tf
+++ Team2/data.tf
@@ -1,9 +1,11 @@
 data "terraform_remote_state" "main" {
   backend = "s3"
   config = {
-    bucket = "test-bucket3-terraform"
-    key    = "path/to/my/key"
-    region = "eu-west-1"
+    bucket             = "bucket-for-s3backendmaverick"     # create s3 in aws
+    #dynamodb_table = "state-lock"                            # create dynamo table in aws
+    key                = "global/mystatefile/terraform.tfstate"
+    region             = "eu-west-1"
+    encrypt            = true
   }
 }
 
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 
 # backend setup block
 terraform {
   backend "s3" {
-    bucket             = "bucket-for-s3backendmaverick"     # create s3 in aws
+    bucket = "bucket-for-s3backendmaverick" # create s3 in aws
     #dynamodb_table = "state-lock"                            # create dynamo table in aws
-    key                = "global/mystatefile/terraform.tfstate"
-    region             = "eu-west-1"
-    encrypt            = true
+    key     = "global/mystatefile/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
   }
 } 
