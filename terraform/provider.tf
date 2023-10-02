356e69e402a245a7a08b9a060ab90b9af818d468
nbmaverick <nbdevops3036@gmail.com>
2023-10-02 20:12:45 +0000

    changed backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 
 # backend setup block
-/* terraform {
+terraform {
   backend "s3" {
     bucket             = "bucket-for-s3backendmaverick"     # create s3 in aws
-    dynamodb_table = "state-lock"                            # create dynamo table in aws
+    #dynamodb_table = "state-lock"                            # create dynamo table in aws
     key                = "global/mystatefile/terraform.tfstate"
     region             = "eu-west-1"
     encrypt            = true
   }
-} */
+} 
