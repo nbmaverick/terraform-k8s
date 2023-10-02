1e34661e0f036642bf82ce76f585ec766dae3d7a
nbmaverick <nbdevops3036@gmail.com>
2023-10-02 20:09:53 +0000

    aded data

--- Team2/data.tf
+++ Team2/data.tf
@@ -1,12 +1,12 @@
 data "terraform_remote_state" "main" {
-    backend = "s3"
-    config = {
+  backend = "s3"
+  config = {
     bucket = "test-bucket3-terraform"
-    key = "path/to/my/key"
+    key    = "path/to/my/key"
     region = "eu-west-1"
-    }
+  }
 }
 
 output "full_list" {
-    value = data.terraform_remote_state.main.outputs.*
+  value = data.terraform_remote_state.main.outputs.*
 }
